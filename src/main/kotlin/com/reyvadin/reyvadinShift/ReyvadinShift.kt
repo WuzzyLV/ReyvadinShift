e4dbafab5c95c7ef5f8d4438fe58119452082601
WuzzyLV <WuzzyLV@users.noreply.github.com>
2026-06-22 01:24:09 +0300

    Init

---
+++ src/main/kotlin/com/reyvadin/reyvadinShift/ReyvadinShift.kt
@@ -0,0 +1,14 @@
+package com.reyvadin.reyvadinShift
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ReyvadinShift : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
